810c92c0b3d49efe48951abae6f63566c144134f
markendroid <markkurtukov@gmail.com>
2021-12-22 03:06:06 +0700

    fix actions

--- spec/game_process_spec.rb
+++ spec/game_process_spec.rb
@@ -1,17 +1,26 @@
 require './lib/game_process'
-
+require 'yaml'
 RSpec.describe GameProcess do
   describe '#GameProcess' do
-    context 'check_action' do
+    stats = {
+      'health' => 100,
+      'mana' => 0,
+      'fun' => 0,
+      'tire' => 0,
+      'money' => 0
+    }
+    context 'choose_action' do
       game = GameProcess.new
-      stats = {
-        'health' => 70,
-        'mana' => 10,
-        'fun' => -10,
-        'tire' => 25,
-        'money' => 2500
+      expected_stats = {
+        'health' => 100,
+        'mana' => -30,
+        'fun' => -5,
+        'tire' => 70,
+        'money' => 100
       }
-      it { expect(game.check_action(stats)).to eq false }
+      game.action_item = 1
+      game.actions_config = YAML.safe_load(File.read('./actions.yml'))
+      it { expect(game.choose_action(stats)).to eq expected_stats }
     end
 
     context 'do_action' do
@@ -24,7 +33,13 @@ RSpec.describe GameProcess do
         'money' => 100
       }
       game.action_item = 1
+      game.actions_config = YAML.safe_load(File.read('./actions.yml'))
       it { expect(game.do_action.stats).to eq expected_stats }
+    end
+
+    context 'check_action' do
+      game = GameProcess.new
+      it { expect(game.check_action(stats)).to eq false }
     end
   end
 end
